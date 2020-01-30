dbe4d6ae31b24e6ac453f82ac6a792b1984c6aa7
alex-khrust <st.alex.ser@gmail.com>
2020-01-30 10:59:30 +0200

    Добавил адаптив для всей страницы

--- app/js/_custom.js
+++ app/js/_custom.js
@@ -16,6 +16,38 @@ document.addEventListener("DOMContentLoaded", function() {
       delay: 5000,
       disableOnInteraction: false,
     },
-  })
+  });
+  
+  //Добавление и удаление классов по ширене экрана  ----------
+  var windowWidth = $(window).width();
+  if (windowWidth < 992) $(".topnav-wrap").addClass("mob-menu");
+  else $(".topnav-wrap").removeClass("mob-menu");
+  
+  $(window).resize(function () {
+    var windowWidth = $(window).width();
+    if (windowWidth < 992) $(".topnav-wrap").addClass("mob-menu");
+    else $(".topnav-wrap").removeClass("mob-menu");
+  });
+//----------------------------------------------------------------------------
+// Hamburger -----------------------------------------
+  $('.hamburger').click(function() {
+    $(this).toggleClass('active');
+    $('.topnav-wrap').toggleClass('open');
+  });
+//----------------------------------------------------------------------------
+  var acc = document.getElementsByClassName("accordion");
+  var i;
+  
+  for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function() {
+      this.classList.toggle("active");
+      var panel = this.nextElementSibling;
+      if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+      } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+      }
+    });
+  }
 
 });
